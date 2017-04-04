07165e0d2403076413941da88ada8cd609eec796
Nicolas Bovee <nic@ghettifish.com>
2017-04-03 23:20:25 -0800

    Fix the notrious march bug of trellis

--- recycles.co/web/app/themes/recyclestheme/templates/template-home.blade.php
+++ recycles.co/web/app/themes/recyclestheme/templates/template-home.blade.php
@@ -4,7 +4,9 @@
 
 @extends('layouts.base')
 
+@section('content')
   @while(have_posts()) @php(the_post())
     @include('partials.home-hero')
     @include('partials.content-page')
   @endwhile
+@endsection
